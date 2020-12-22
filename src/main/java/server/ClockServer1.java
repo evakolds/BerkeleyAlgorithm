c0c4cee8f11c8d23f76cd24891761c703f4605de
Yeva Koldovska <evakoldovskaya@gmail.com>
2020-12-22 14:04:29 +0200

    minor fixes #2

--- src/main/java/server/ClockServer1.java
+++ src/main/java/server/ClockServer1.java
@@ -12,7 +12,7 @@ public class ClockServer1 {
 
 	public static void main(String[] args) {
 		try {
-			ClockServer clockServer1 = new ClockServerImpl(LocalTime.parse("13:40:26", formatter));
+			ClockServer clockServer1 = new ClockServerImpl(LocalTime.parse("13:40:00", formatter));
 			Registry registry1 = LocateRegistry.createRegistry(AppConstants.SERVER_PORT_1);
 			registry1.rebind(ClockServerImpl.class.getSimpleName(), clockServer1);
 			System.out.println(String.format("Server 1 is running on port %s", AppConstants.SERVER_PORT_1));
